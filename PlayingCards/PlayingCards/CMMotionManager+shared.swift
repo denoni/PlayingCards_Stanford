813d0da430f9b80a7ad3435ce2037181751ece79
Gabriel <gabrieldenonifake@gmail.com>
2021-12-12 23:35:12 -0300

    Add CMMotionManager singleton extension

---
+++ PlayingCards/PlayingCards/CMMotionManager+shared.swift
@@ -0,0 +1,12 @@
+//
+//  CMMotionManager+shared.swift
+//  PlayingCards
+//
+//  Created by Gabriel on 12/12/21.
+//
+
+import CoreMotion
+
+extension CMMotionManager {
+  static var shared = CMMotionManager()
+}
